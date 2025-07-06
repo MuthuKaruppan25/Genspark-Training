d93a56303bb0978f67c41344edc010e056c7baa6
MuthuKaruppan25 <muthukaruppanaakash76@gmail.com>
2025-07-06 20:21:02 +0530

    Completed Tasks

---
+++ DockerTasks/Task-4/Frontend/Task-3/my-react/src/App.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000") 
+      .then((res) => res.text())
+      .then((data) => setMessage(data));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{message}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
